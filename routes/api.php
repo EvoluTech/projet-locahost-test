812d6363a84895bd2642a93528749e20f9d624f3
Dinamihaja <dinamihf1414@gmail.com>
2023-07-28 11:23:13 +0300

    CRUD Biens Vita a

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestApi;
+use App\Http\Controllers\Biens;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,6 +18,11 @@ Route::post('/login', [TestApi::class, 'login']);
 Route::post('/creerCompte', [TestApi::class, 'creerCompte']);
 Route::delete('/delete/{id_user}', [TestApi::class, 'destroy']);
 Route::put('/update/{id_user}', [TestApi::class, 'update']);
+
+Route::post('/creerBien', [Biens::class, 'creerBien']);
+Route::delete('/suprimer/{id_objet}', [Biens::class, 'suprimer']);
+Route::put('/maj/{id_objet}', [Biens::class, 'maj']);
+Route::get('recherche/{id_objet}',  [Biens::class, 'recherche']);
 
 Route::middleware('verify.token')->group(function(){
 
