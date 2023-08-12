d5a2b1c5ec59ccdcea2d622ae99e8916af681c1f
Dinamihaja <dinamihf1414@gmail.com>
2023-08-12 12:24:57 +0300

    bien postuler test api

--- app/Http/Controllers/TestApi.php
+++ app/Http/Controllers/TestApi.php
@@ -6,9 +6,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Carbon;
 
 class TestApi extends Controller
 {
@@ -20,6 +23,10 @@ class TestApi extends Controller
     public function __construct()
     {
         //middleware = polisy misava ticket
+    }
+    function convStringTimestamp($dateString) {
+        $date = Carbon::createFromFormat('d/m/Y', $dateString);
+        return $date;
     }
 
     public function login(Request $request)
@@ -315,6 +322,124 @@ class TestApi extends Controller
             ], 200
         );
     }
+
+
+
+
+    public function getNextSequenceValue()
+{
+    // Exécutez la requête pour récupérer la prochaine valeur de la séquence
+    $result = DB::select("SELECT nextval('id_detail_bien_postuler') as next_value");
+
+    // Renvoyez la valeur générée en tant que réponse JSON
+    return response()->json(['next_value' => $result[0]->next_value], 200);
+}
+
+
+
+public function BiensPostuler(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+
+            'date_debut_postule' => 'required',
+            'date_fin_postule' => 'required',
+            'prix_biens' => 'required',
+            'prix_par_jour' => 'required',
+            'prix_total_payer' => 'required',
+            'etat_biens' => 'required',
+            'description_biens' => 'required',
+            'ville' => 'required',
+            'id_objet' => 'required',
+
+        ], [
+            'required' => 'Le champ :attribute est obligatoire.',
+            'string' => 'Le champ :attribute doit être une chaîne de caractères.',
+            'max' => 'Le champ :attribute ne doit pas dépasser :max caractères.',
+            'min' => 'Le champ :attribute doit contenir au moins :min caractères.',
+        ]);
+
+        if ($validator->fails())
+        {
+            // Renvoyer une réponse JSON avec les erreurs de validation
+            return response()->json
+            (
+                [
+                    'errors' => $validator->errors()
+                ], 422
+            );
+        }
+
+
+        $date_debut_postule = $this->convStringTimestamp($request->input('date_debut_postule'));
+        $date_fin_postule = $this->convStringTimestamp($request->input('date_fin_postule'));
+        $prix_biens = $request->input('prix_biens');
+        $prix_par_jour = $request->input('prix_par_jour');
+        $prix_total_payer = $request->input('prix_total_payer');
+        $etat_biens = $request->input('etat_biens');
+        $description_biens = $request->input('description_biens');
+        $ville = $request->input('ville');
+        $id_objet = $request->input('id_objet');
+
+        $result = DB::select("SELECT nextval('id_detail_bien_postuler') as next_value");
+        $sequence_id_detail_bien = $result[0]->next_value;
+
+        $nombre_vue_detailsbienspostuler = 0;
+        $signalisation_detailsbienspostuler = 0;
+        $status_detailsbienspostuler = 0;
+
+        $dataDetailsB=[$sequence_id_detail_bien,$nombre_vue_detailsbienspostuler,$signalisation_detailsbienspostuler,$status_detailsbienspostuler];
+        $sqlInsert="INSERT INTO public.detailsbienspostuler(id_detailsbienspostuler,nombre_vue_detailsbienspostuler,signalisation_detailsbienspostuler,status_detailsbienspostuler)
+        values (?,?,?,?)";
+        try {
+            $insert = DB::insert($sqlInsert,$dataDetailsB);
+
+
+            $insertion = DB::insert(
+                "INSERT INTO public.bienspostuler(
+                    id_bienspostuler,
+                    date_debut_postule,
+                    date_fin_postule,
+                    prix_biens,
+                    prix_par_jour,
+                    prix_total_payer,
+                    etat_biens,
+                    description_biens,
+                    ville,
+                    id_objet,
+                    id_detailsbienspostuler)
+                VALUES (
+                    nextval('id_bien_postuler'),
+                    '$date_debut_postule', -- Utilisation de la variable
+                    '$date_fin_postule',   -- Utilisation de la variable
+                    '$prix_biens',         -- Utilisation de la variable
+                    '$prix_par_jour',      -- Utilisation de la variable
+                    '$prix_total_payer',   -- Utilisation de la variable
+                    '$etat_biens',         -- Utilisation de la variable
+                    '$description_biens',  -- Utilisation de la variable
+                    '$ville',              -- Utilisation de la variable
+                    '$id_objet' ,           -- Utilisation de la variable
+                    '$sequence_id_detail_bien'-- Utilisation de la variable
+                );"
+            );
+
+            return response()->json(
+                [
+                    "status" => true,
+                    "message" => "Insertion réussie"
+                ], 200
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    "status" => false,
+                    "erreur" => $th,
+                    "message" => "Insertion non réussie"
+                ], 400
+            );
+        }
+}
+
+
 
 
 
